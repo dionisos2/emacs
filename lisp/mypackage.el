59fd20e69029b7995f6e7d5678c1338537e9d604
dionisos <denis.baudouin@gmail.com>
2016-10-09 11:28:00 +0200

    mypackage.el

--- lisp/mypackage.el
+++ lisp/mypackage.el
@@ -1,9 +1,9 @@
 (require 'package)
 
-;; (add-to-list 'package-archives
-;;              '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 (add-to-list 'package-archives
-			 '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+	     '("melpa-stable" . "http://stable.melpa.org/packages/") t)
 
 (package-initialize)
