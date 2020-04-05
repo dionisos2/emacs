52accda4e8d83cbc27b7eabc8a71312eec35518f
dionisos <denis.baudouin@gmail.com>
2020-04-05 13:58:15 +0200

    Change function name

--- lisp/my-macro.el
+++ lisp/my-macro.el
@@ -6,7 +6,7 @@
 ;; 4) call macro as a function
 ;;; Code:
 
-(fset 'rotate_date
+(fset 'my-rotate-date
    (lambda (&optional arg) "Keyboard macro." (interactive "p") (kmacro-exec-ring-item (quote ([134217843 tab 5 134217836 24 24 1 134217828 tab 5 134217836 67108910 1 tab 134217843 tab] 0 "%d")) arg)))
 
 
