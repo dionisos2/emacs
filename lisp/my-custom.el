2dd362f1bd95fef03b7393df408719c3aafd04fe
dionisos <denis.baudouin@gmail.com>
2020-11-25 12:24:06 +0100

    Random stuffs

--- lisp/my-custom.el
+++ lisp/my-custom.el
@@ -10,8 +10,8 @@
  '(c-default-style '((c-mode . "haypo") (c++-mode . "haypo")))
  '(column-number-mode t)
  '(company-backends
-	 '((company-dabbrev-code company-gtags company-etags company-keywords company-abbrev company-dabbrev)
-		 company-dabbrev))
+   '((company-dabbrev-code company-gtags company-etags company-keywords company-abbrev company-dabbrev)
+	 company-dabbrev))
  '(company-dabbrev-char-regexp "\\(\\sw\\|[-$_]\\)")
  '(company-dabbrev-downcase nil)
  '(company-dabbrev-ignore-case nil)
@@ -21,7 +21,7 @@
  '(delete-by-moving-to-trash t)
  '(delete-selection-mode 1)
  '(describe-char-unidata-list
-	 '(name old-name general-category canonical-combining-class bidi-class decomposition decimal-digit-value))
+   '(name old-name general-category canonical-combining-class bidi-class decomposition decimal-digit-value))
  '(dired-hide-details-hide-information-lines nil)
  '(dired-hide-details-hide-symlink-targets nil)
  '(dired-listing-switches "-alh")
@@ -37,10 +37,11 @@
  '(ein:worksheet-enable-undo t)
  '(elpy-eldoc-show-current-function nil)
  '(elpy-modules
-	 '(elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults))
+   '(elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-django elpy-module-sane-defaults))
  '(find-file-visit-truename t)
+ '(flycheck-checker-error-threshold 1000)
  '(flycheck-checkers
-	 '(my-php-checker my-php-phpcs ada-gnat asciidoc c/c++-clang c/c++-gcc c/c++-cppcheck cfengine chef-foodcritic coffee coffee-coffeelint coq css-csslint d-dmd emacs-lisp emacs-lisp-checkdoc erlang eruby-erubis fortran-gfortran go-gofmt go-golint go-vet go-build go-test go-errcheck go-unconvert groovy haml handlebars haskell-stack-ghc haskell-ghc haskell-hlint html-tidy jade javascript-jshint javascript-standard javascript-eslint javascript-gjslint javascript-jscs json-jsonlint json-python-json less lua-luacheck lua perl perl-perlcritic php php-phpmd php-phpcs processing puppet-parser puppet-lint python-pylint python-flake8 python-pycompile r-lintr racket rpm-rpmlint markdown-mdl rst-sphinx rst ruby-rubocop ruby-rubylint ruby ruby-jruby rust-cargo rust sass scala scala-scalastyle scheme-chicken scss-lint scss sh-bash sh-posix-dash sh-posix-bash sh-zsh sh-shellcheck slim slim-lint sql-sqlint tex-chktex tex-lacheck texinfo typescript-tslint verilog-verilator xml-xmlstarlet xml-xmllint yaml-jsyaml yaml-ruby))
+   '(my-php-checker my-php-phpcs ada-gnat asciidoc c/c++-clang c/c++-gcc c/c++-cppcheck cfengine chef-foodcritic coffee coffee-coffeelint coq css-csslint d-dmd emacs-lisp emacs-lisp-checkdoc erlang eruby-erubis fortran-gfortran go-gofmt go-golint go-vet go-build go-test go-errcheck go-unconvert groovy haml handlebars haskell-stack-ghc haskell-ghc haskell-hlint html-tidy jade javascript-jshint javascript-standard javascript-eslint javascript-gjslint javascript-jscs json-jsonlint json-python-json less lua-luacheck lua perl perl-perlcritic php php-phpmd php-phpcs processing puppet-parser puppet-lint python-pylint python-flake8 python-pycompile r-lintr racket rpm-rpmlint markdown-mdl rst-sphinx rst ruby-rubocop ruby-rubylint ruby ruby-jruby rust-cargo rust sass scala scala-scalastyle scheme-chicken scss-lint scss sh-bash sh-posix-dash sh-posix-bash sh-zsh sh-shellcheck slim slim-lint sql-sqlint tex-chktex tex-lacheck texinfo typescript-tslint verilog-verilator xml-xmlstarlet xml-xmllint yaml-jsyaml yaml-ruby))
  '(flycheck-emacs-lisp-load-path 'inherit)
  '(global-flycheck-mode t)
  '(helm-bookmark-show-location t)
@@ -65,7 +66,7 @@
  '(minibuffer-prompt-properties '(read-only t cursor-intangible t face minibuffer-prompt))
  '(openwith-associations '((".pdf" "evince" (file)) (".docx" "libreoffice" (file))))
  '(org-agenda-files
-	 '("~/organisation/agenda.org" "~/organisation/birthdays.org"))
+   '("~/organisation/agenda.org" "~/organisation/birthdays.org"))
  '(org-agenda-include-diary t)
  '(org-agenda-restore-windows-after-quit t)
  '(org-agenda-window-setup 'current-window)
@@ -74,16 +75,16 @@
  '(org-log-done nil)
  '(org-log-repeat nil)
  '(org-modules
-	 '(org-bbdb org-bibtex org-docview org-gnus org-info org-irc org-mhe org-rmail org-w3m org-checklist))
+   '(org-bbdb org-bibtex org-docview org-gnus org-info org-irc org-mhe org-rmail org-w3m org-checklist))
  '(org-startup-truncated nil)
  '(org-tags-column 100)
  '(org-tags-exclude-from-inheritance '("ORDERED"))
  '(org-track-ordered-property-with-tag t)
  '(package-archives
-	 '(("melpa" . "http://melpa.org/packages/")
-		 ("gnu" . "http://elpa.gnu.org/packages/")))
+   '(("melpa" . "http://melpa.org/packages/")
+	 ("gnu" . "http://elpa.gnu.org/packages/")))
  '(package-selected-packages
-	 '(kmb flycheck-elm flycheck-haskell json-mode elpy helm-dash key-chord markdown-mode ein dired-quick-sort fish-mode openwith s helm-swoop web-beautify company-lua company-math company-php company-statistics elm-mode helm-company bind-key realgud which-key picpocket anchored-transpose geben julia-mode lua-mode python-mode web-mode magit ace-jump-mode auctex flycheck graphviz-dot-mode langtool pylint undo-tree yaml-mode php-mode spray dired-narrow dired+))
+   '(kmb flycheck-elm flycheck-haskell json-mode elpy helm-dash key-chord markdown-mode ein dired-quick-sort fish-mode openwith s helm-swoop web-beautify company-lua company-math company-php company-statistics elm-mode helm-company bind-key realgud which-key picpocket anchored-transpose geben julia-mode lua-mode python-mode web-mode magit ace-jump-mode auctex flycheck graphviz-dot-mode langtool pylint undo-tree yaml-mode php-mode spray dired-narrow dired+))
  '(pdf-view-continuous t)
  '(printer-name "EPSON_WF-2750")
  '(python-indent-guess-indent-offset nil)
@@ -93,8 +94,9 @@
  '(python-shell-interpreter-args "")
  '(realgud:pdb-command-name "python -m pdb")
  '(safe-local-variable-values
-	 '((eval setq byte-compile-not-obsolete-vars
-					 '(display-buffer-function))))
+   '((py-indent-offset . 4)
+	 (eval setq byte-compile-not-obsolete-vars
+		   '(display-buffer-function))))
  '(savehist-mode t)
  '(scroll-conservatively 99)
  '(scroll-step 1)
@@ -122,7 +124,7 @@
  '(yank-excluded-properties t)
  '(yas-indent-line 'fixed)
  '(yas-snippet-dirs
-	 '("/home/dionisos/.emacs.d/snippets/snippets" "/home/dionisos/.emacs.d/elpa/elpy-20191002.2144/snippets/")))
+   '("/home/dionisos/.emacs.d/snippets/snippets" "/home/dionisos/.emacs.d/elpa/elpy-20191002.2144/snippets/")))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
