6c6d04d2b9af998c71cd12b951c88e5e2dab11e2
dionisos2 <denis.baudouin@gmail.com>
2022-12-28 13:11:32 +0100

    "Save"

--- lisp/my-custom.el
+++ lisp/my-custom.el
@@ -3,10 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ement-room-image-initial-height nil)
- '(ement-room-retro-messages-number 300)
  '(package-selected-packages
-	 '(emojify ement yasnippet xterm-color xonsh-mode which-key wgrep w3m visual-regexp-steroids vertico use-package-chords undo-tree transpose-mark spray smart-tabs-mode semi selectrum rust-mode realgud ranger php-mode phi-search org-roam orderless openwith notmuch multiple-cursors marginalia magit lsp-ui lsp-pyright langtool kiwix julia-repl julia-mode jsonrpc highlight-indent-guides haskell-mode google-translate flyspell-correct flymake flycheck embark-consult emacsql-sqlite3 elquery elfeed dap-mode csharp-mode corfu consult-lsp consult-company company-statistics company-quickhelp company-prescient all-the-icons-dired abyss-theme)))
+	 '(yasnippet xterm-color xonsh-mode which-key wgrep w3m visual-regexp-steroids vertico use-package-chords undo-tree transpose-mark spray smart-tabs-mode semi selectrum rust-mode realgud ranger php-mode phi-search org-roam orderless openwith notmuch multiple-cursors marginalia magit lsp-ui lsp-pyright langtool kiwix julia-repl julia-mode jsonrpc highlight-indent-guides haskell-mode google-translate flyspell-correct flymake flycheck emojify ement embark-consult emacsql-sqlite3 elquery elfeed dap-mode csharp-mode corfu consult-lsp consult-company company-statistics company-quickhelp company-prescient all-the-icons-dired abyss-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
